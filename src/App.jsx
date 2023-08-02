50aac09b2561a36d50e64ff05e9413d24f28420d
josedasilva123 <55949988+josedasilva123@users.noreply.github.com>
2023-08-02 15:24:02 -0300

    atividade4

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
+import { Button } from "./components/Button"
+
 function App() {
   return (
     <>
-
+      <Button className="btn" type="button" disabled={true}>
+        Teste
+      </Button>
     </>
   )
 }
